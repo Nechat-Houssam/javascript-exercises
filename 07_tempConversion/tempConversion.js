a8626d2ac5e94893cee60981883624525c909472
Houssam <houssam.nechat@dauphine.eu>
2023-06-02 20:28:35 +0200

    exo js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+
+      let num = ((x - 32) * (5/9));
+
+      if ( num % 1 == 0)
+         return num
+
+      else 
+         return parseFloat(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+
+      let num = (x * (9/5) + 32);
+
+      if ( num % 1 == 0)
+         return num
+
+      else 
+         return parseFloat(num.toFixed(1));
 };
 
 // Do not edit below this line
